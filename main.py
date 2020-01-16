57ee1f6f864e57c0e07a7214a81a0861132bed6a
Acrom <zvez.dmtr@gmail.com>
2020-01-16 12:29:39 +1000

    Add .gitignore, add storage of NN, reformat code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import neuro
+import pickle
+import os.path
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,29 +8,34 @@ data_file = open("mnist_test.csv", 'r')
 data_list = data_file.readlines()
 data_file.close()
 
-input_nodes = 28*28
+input_nodes = 28 * 28
 hidden_nodes = 200
 output_nodes = 10
 learning_rate = 0.15
 
+if os.path.isfile('neuro.pickle'):
+	with open('neuro.pickle', 'rb') as f:
+		n = pickle.load(f)
+else:
+	n = neuro.NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+	training_data_file = open("mnist_train.csv", 'r')
+	training_data_list = training_data_file.readlines()
+	training_data_file.close()
 
-n = neuro.NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
-training_data_file = open("mnist_train.csv", 'r')
-training_data_list = training_data_file.readlines()
-training_data_file.close()
+	epochs = 1
+	for e in range(epochs):
+		for record in training_data_list:
+			all_values = record.split(',')
+			inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+			targets = np.zeros(output_nodes) + 0.01
+			targets[int(all_values[0])] = 0.99
+			n.train(inputs, targets)
+	with open('neuro.pickle', 'wb') as f:
+		pickle.dump(n, f)
 
-epochs = 1
-for e in range(epochs):
-    for record in training_data_list:
-        all_values = record.split(',')
-        inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-        targets = np.zeros(output_nodes) + 0.01
-        targets[int(all_values[0])] = 0.99
-        n.train(inputs, targets)
-
-m = 2 # номер элемента в контрольной выборке, для которого хочешь найти класс
+m = 2  # номер элемента в контрольной выборке, для которого хочешь найти класс
 all_values = data_list[m].split(',')
-image_array = np.asfarray(all_values[1:]).reshape((28,28))
+image_array = np.asfarray(all_values[1:]).reshape((28, 28))
 print(all_values[0])
 plt.imshow(image_array, cmap='Greys', interpolation='None')
 inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
--- neuro.py
+++ neuro.py
@@ -1,53 +1,55 @@
 import numpy as np
 import scipy.special as sci
 
-class NeuralNetwork:
-    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
-        # количество входных, скрытых, выходных узлов
-        self.inodes = input_nodes
-        self.hnodes = hidden_nodes
-        self.onodes = output_nodes
-        # коэффициент обучения
-        self.lr = learning_rate
-        # матрицы весов
-        #self.wih = (np.random.rand(self.hnodes, self.inodes)- 0.5)
-        #self.who = (np.random.rand(self.onodes, self.hnodes)- 0.5)
-        #self.wih = (np.random.rand(self.hnodes, self.inodes)*2.0 - 1.0)
-        #self.who = (np.random.rand(self.onodes, self.hnodes)*2.0 - 1.0)
-        self.wih = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
-        self.who = np.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
-        # функция активации (сигмоида)
-        # self.activation_function = lambda x: sci.expit(x)
-        pass
-    
-    def activation_function(self, x):
-        return sci.expit(x)
 
-    def train(self, inputs_list, targets_list):
-        inputs = np.array(inputs_list, ndmin=2).T
-        targets = np.array(targets_list, ndmin=2).T
-        
-        hidden_inputs = np.dot(self.wih, inputs)
-        hidden_outputs = self.activation_function(hidden_inputs)
-        
-        final_inputs = np.dot(self.who, hidden_outputs)
-        final_outputs = self.activation_function(final_inputs)
-        
-        output_errors = targets - final_outputs
-        hidden_errors = np.dot(self.who.T, output_errors)
-        
-        self.who += self.lr * np.dot((output_errors * final_outputs * (1 - final_outputs)),np.transpose(hidden_outputs))
-        self.wih += self.lr * np.dot((hidden_errors * hidden_outputs * (1 - hidden_outputs)),np.transpose(inputs))
-        pass
-    
-    def query(self, inputs_list):
-        # преобразовать список входных значений
-        # в двухмерный массив
-        inputs = np.array(inputs_list, ndmin=2).T
-        
-        hidden_inputs = np.dot(self.wih, inputs)
-        hidden_outputs = self.activation_function(hidden_inputs)
-        
-        final_inputs = np.dot(self.who, hidden_outputs)
-        final_outputs = self.activation_function(final_inputs)
-        return final_outputs
+class NeuralNetwork:
+	def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+		# количество входных, скрытых, выходных узлов
+		self.inodes = input_nodes
+		self.hnodes = hidden_nodes
+		self.onodes = output_nodes
+		# коэффициент обучения
+		self.lr = learning_rate
+		# матрицы весов
+		# self.wih = (np.random.rand(self.hnodes, self.inodes)- 0.5)
+		# self.who = (np.random.rand(self.onodes, self.hnodes)- 0.5)
+		# self.wih = (np.random.rand(self.hnodes, self.inodes)*2.0 - 1.0)
+		# self.who = (np.random.rand(self.onodes, self.hnodes)*2.0 - 1.0)
+		self.wih = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+		self.who = np.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+		# функция активации (сигмоида)
+		# self.activation_function = lambda x: sci.expit(x)
+		pass
+
+	def activation_function(self, x):
+		return sci.expit(x)
+
+	def train(self, inputs_list, targets_list):
+		inputs = np.array(inputs_list, ndmin=2).T
+		targets = np.array(targets_list, ndmin=2).T
+
+		hidden_inputs = np.dot(self.wih, inputs)
+		hidden_outputs = self.activation_function(hidden_inputs)
+
+		final_inputs = np.dot(self.who, hidden_outputs)
+		final_outputs = self.activation_function(final_inputs)
+
+		output_errors = targets - final_outputs
+		hidden_errors = np.dot(self.who.T, output_errors)
+
+		self.who += self.lr * np.dot((output_errors * final_outputs * (1 - final_outputs)),
+									 np.transpose(hidden_outputs))
+		self.wih += self.lr * np.dot((hidden_errors * hidden_outputs * (1 - hidden_outputs)), np.transpose(inputs))
+		pass
+
+	def query(self, inputs_list):
+		# преобразовать список входных значений
+		# в двухмерный массив
+		inputs = np.array(inputs_list, ndmin=2).T
+
+		hidden_inputs = np.dot(self.wih, inputs)
+		hidden_outputs = self.activation_function(hidden_inputs)
+
+		final_inputs = np.dot(self.who, hidden_outputs)
+		final_outputs = self.activation_function(final_inputs)
+		return final_outputs
